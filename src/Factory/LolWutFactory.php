2587520974801b52e025ab62aa1ac3e9cda4fe76
Joshua Copeland <joshuarcopeland@gmail.com>
2017-09-07 14:19:33 -0700

    LOL WUT?

---
+++ src/Factory/LolWutFactory.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: josh
+ * Date: 9/7/17
+ * Time: 2:13 PM
+ */
+
+namespace OGP\Factory;
+
+use OGP\LolWut;
+
+class LolWutFactory {
+    static function getLolWut()
+    {
+        return new LolWut();
+    }
+}
---
+++ src/LolWut.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OGP;
+
+/**
+ * Class lolwut
+ * @package OGP
+ */
+class LolWut {
+
+    /**
+     * Can I haz funny plox?
+     * @return string
+     */
+    public function getFunny()
+    {
+        return 'LOL Wut';
+    }
+}
